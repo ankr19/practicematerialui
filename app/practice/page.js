3867c1ccd782fee975f46812d762f22613002f28
anandkumar-1998 <anand.kumar@nidester.com>
2024-01-18 14:37:09 +0530

    updated

---
+++ app/practice/page.js
@@ -0,0 +1,14 @@
+import Testing from '@/components/Testing'
+import React from 'react'
+export const metadata = {
+    title: "Practice the mui"
+}
+const Page = () => {
+  return (
+    <div>
+      <Testing/>
+    </div>
+  )
+}
+
+export default Page
---
+++ components/Testing.jsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import { MdKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { Avatar } from "@mui/material";
+
+const Testing = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="fade-button"
+        aria-controls={open ? "fade-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        // variant="contained"
+        endIcon={open ? <MdOutlineKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+      >
+        {/* Dashboard */}
+        <Avatar sx={{ width: 32, height: 32, backgroundColor:'black',border:"2px solid black" }}>A</Avatar>
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          "aria-labelledby": "fade-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default Testing;
