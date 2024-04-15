f525c7b508092a2033dc955ed4f6458b4a0b5765
anand kumar <ankm335@gmail.com>
2024-04-15 22:57:37 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -15,42 +15,40 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import HomeIcon from '@mui/icons-material/Home';
-import StarIcon from '@mui/icons-material/Star';
-import ChecklistIcon from '@mui/icons-material/Checklist';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SupportIcon from '@mui/icons-material/Support';
-import LogoutIcon from '@mui/icons-material/Logout';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import HomeIcon from "@mui/icons-material/Home";
+import StarIcon from "@mui/icons-material/Star";
+import ChecklistIcon from "@mui/icons-material/Checklist";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SupportIcon from "@mui/icons-material/Support";
+import LogoutIcon from "@mui/icons-material/Logout";
 import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 const DRAWER_WIDTH = 240;
 import { usePathname } from "next/navigation";
 
 const LINKS = [
-  { text: 'Home', href: '/', icon: HomeIcon },
-  { text: 'Starred', href: '/starred', icon: StarIcon },
-  { text: 'Tasks', href: '/tasks', icon: ChecklistIcon },
+  { text: "Home", href: "/", icon: HomeIcon },
+  { text: "Starred", href: "/starred", icon: StarIcon },
+  { text: "Tasks", href: "/tasks", icon: ChecklistIcon },
 ];
 
 const PLACEHOLDER_LINKS = [
-  { text: 'Settings', icon: SettingsIcon },
-  { text: 'Support', icon: SupportIcon },
-  { text: 'Logout', icon: LogoutIcon },
+  { text: "Settings", icon: SettingsIcon },
+  { text: "Support", icon: SupportIcon },
+  { text: "Logout", icon: LogoutIcon },
 ];
 export const metadata = {
   title: "Practice",
   description: "Practing the mui with tailwindcss values",
 };
 
-export default function RootLayout({children}){
-return(
-  <html lang="en">
-    <body>
-      <ThemeRegistry>
-      {children}
-      </ThemeRegistry>
-    </body>
-  </html>
-)
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeRegistry>{children}</ThemeRegistry>
+      </body>
+    </html>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Layout1 from '@/components/Layout/Layout1'
 import Layout2 from '@/components/Layout/Layout2'
+import Layout3 from '@/components/Layout/Layout3'
 import React from 'react'
 export default function Home() {
   return (
-    <Layout2/>
+    // <Layout2/>
+    <Layout3/>
   )
 }
